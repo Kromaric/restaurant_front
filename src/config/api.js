860c324a553e99dd38d7a37902a68134e9648e2e
laurent papinaud <laurent.papinaud@gmail.com>
2026-02-11 18:52:01 +0100

    Hotfix: corrected URL

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
 // REMPLACE PAR TON URL CLOUD RUN
 // export const API_URL = 'https://my-crud-app-912210011044.europe-west1.run.app';
-export const API_URL = 'https://my-crud-app-450184488008.europe-west1.run.app/';
+export const API_URL = 'https://my-crud-app-450184488008.europe-west1.run.app';
